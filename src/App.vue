f707aca0fae8f47b8ac7700ac4e42d791cb727b6
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2023-02-07 10:11:40 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+.btn{
+  @apply bg-purple-500 text-indigo-50 px-4 rounded-full hover:(bg-purple-600);
+}
+</style>
